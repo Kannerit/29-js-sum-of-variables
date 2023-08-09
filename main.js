4a7baee1d854abe09d5a972b8a6951df31928e5d
Karen <kanneritg@gmail.com>
2023-08-09 12:46:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstNumber = 7;
+let secondNumber = 4;
+
+function add(number1, number2){
+  let result = number1 + number2;
+
+  return result;
+
+}
+
+console.log(add(firstNumber,secondNumber));
